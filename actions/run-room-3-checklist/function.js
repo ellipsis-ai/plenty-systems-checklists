a8d6f52c25903e89e549c7336c75eeb7b9aa14ac
Andrew Catton <>
2018-10-16 13:16:18 +0000

    Added one more question

--- actions/run-room-3-checklist/function.js
+++ actions/run-room-3-checklist/function.js
@@ -1,11 +1,12 @@
-function(emitters, noWater, growLightsOn, sockFilters, channels, ellipsis) {
+function(emitters, noWater, growLightsOn, sockFilters, middleRack, channels, ellipsis) {
   const support = require('support')(ellipsis);
 
 const questionsAndAnswers = {
   "Emitters flowing": emitters,
   "Confirmed there is no water on the floor": noWater,
   "Grow lights on (OFF 10am-4pm)": growLightsOn,
-  "Confirmed sock filters are draining properly and not ballooning": sockFilters
+  "Confirmed sock filters are draining properly and not ballooning": sockFilters,
+  "Made sure middle light rack is in line": middleRack
 }
 support.postFor("R & D room 3", questionsAndAnswers, channels);
 }
