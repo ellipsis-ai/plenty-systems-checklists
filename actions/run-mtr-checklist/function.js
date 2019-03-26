072a2ad9ca2fe3e7a52d8fc165ca9d1d498ade60
Luke Andrews <luke@attaboy.ca>
2019-03-26 19:40:16 +0000

    Updated grow light times for Megalodon and MTR, and switched sock filter to bucket filter

--- actions/run-mtr-checklist/function.js
+++ actions/run-mtr-checklist/function.js
@@ -1,12 +1,12 @@
-function(emitters, gutters, growLightsOnAuto, overheadLights, checkedSockFilter, checkedBasket, nutrientTankFull, channels, ellipsis) {
+function(emitters, gutters, growLightsOnAuto, overheadLights, checkedBucketFilter, checkedBasket, nutrientTankFull, channels, ellipsis) {
   const support = require('support')(ellipsis);
 
 const questionsAndAnswers = {
   "Emitters flowing": emitters,
   "Gutters draining": gutters,
-  "Grow lights on auto (off 6am to 12pm)": growLightsOnAuto,
+  "Grow lights on auto (off 5am to 8am)": growLightsOnAuto,
   "Overhead room lights only on during production activities (M,W,F)": overheadLights,
-  "Checked sock filter and replaced if necessary": checkedSockFilter,
+  "Checked bucket filter and replaced if necessary": checkedBucketFilter,
   "Checked basket and screen filters and replaced if necessary": checkedBasket,
   "Nutrient Tank #2 is full": nutrientTankFull
 }
