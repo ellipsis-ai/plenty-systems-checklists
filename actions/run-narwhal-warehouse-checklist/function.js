1a3a650883993562ebbd0c3270178ba3853b61c3
Andrew Catton <>
2019-04-04 18:22:18 +0000

    Some updates for narwhal checklist

--- actions/run-narwhal-warehouse-checklist/function.js
+++ actions/run-narwhal-warehouse-checklist/function.js
@@ -1,11 +1,12 @@
-function(waterLevelHighEnough, waterLevelNotTooHigh, buckets, noWaterLeaks, channels, ellipsis) {
+function(waterLevelHighEnough, waterLevelNotTooHigh, buckets, noWaterLeaks, depthSensors, channels, ellipsis) {
   const support = require('support')(ellipsis);
 
 const questionsAndAnswers = {
   "Zone 1, 2, and 3 supply tank water levels at or above 300mm": waterLevelHighEnough,
   "Zone 1, 2, and 3 supply tank water levels not excessively high": waterLevelNotTooHigh,
   "pH down buckets are at least 1/2 full": buckets,
-  "No leaks/water around tanks/filtration/sensor manifold": noWaterLeaks
+  "No leaks/water around tanks/filtration/sensor manifold": noWaterLeaks,
+  "Zone 2 and 3 depth sensors are free of debris": depthSensors
 }
 support.postFor("Narwhal warehouse", questionsAndAnswers, channels);
 }
