4ed0c68b8758b06cc4358ccace695e78f472d2fc
Luke Andrews <luke@ellipsis.ai>
2019-07-30 17:44:57 +0000

    Update grow light off times for MTR/Megalodon

--- actions/run-mtr-checklist/function.js
+++ actions/run-mtr-checklist/function.js
@@ -4,7 +4,7 @@ function(channels, emitters, gutters, growLightsOnAuto, overheadLights, filtersC
 const questionsAndAnswers = {
   "Emitters flowing": emitters,
   "Gutters draining": gutters,
-  "Grow lights on auto (off 7am to 10am)": growLightsOnAuto,
+  "Grow lights on auto (OFF 8am to 11am)": growLightsOnAuto,
   "Overhead room lights only on during production activities (M,W,F)": overheadLights,
   "Filters are clean": filtersClean,
   "Nutrient Tank #2 is full": nutrientTankFull
