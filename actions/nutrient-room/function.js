e659a51634745bc79d2f2fb87bb43926c148c7fc
Andrew Catton <>
2019-09-12 13:36:12 +0000

    New Nutrient Room Checklist + some tweaks previously not committed

---
+++ actions/nutrient-room/function.js
@@ -0,0 +1,22 @@
+function(ellipsis) {
+  const support = require('support')(ellipsis);
+
+const intro = `
+:orca: Hello, this is the Nutrient Room Checklist reminder. 
+
+Anyone in this channel can respond. Start by clicking below.
+`;
+const channelId = ellipsis.event.message ? (ellipsis.event.message.channel ? ellipsis.event.message.channel.id : "") : ""
+ellipsis.success(intro, {
+  choices: [
+    {
+      label: "Start…",
+      actionName: "run-nutrient-room-checklist",
+      args: [
+        { name: "channels", value: channelId }
+      ],
+      allowOthers: true
+    }
+  ]
+});
+}
